dc054182e8ddbafaaa458faf5c3a9b6be20636d1
Jonathan Schoreels <jonathan.schoreels@gmail.com>
2019-03-12 21:52:50 +0100

    Adds Gradle build file

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    `build-scan`
+    kotlin("jvm") version "1.3.21"
+}
+
+group = "be.jschoreels.strengthtrainer"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val test by tasks.getting(Test::class) {
+    useJUnitPlatform { }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test"))
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.1")
+}
